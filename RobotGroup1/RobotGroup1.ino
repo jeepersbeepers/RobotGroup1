d6b98260a446c3ff578f694ff27273150244de4d
lachimolala-109 <95944246+lachimolala-109@users.noreply.github.com>
2024-05-01 11:39:56 -0400

    Initial upload

---
+++ RobotGroup1/RobotGroup1.ino
@@ -0,0 +1,17 @@
+#define IR_1 2
+#define IR_2 4
+#define IR_3 7
+#define IR_4 8
+
+void setup() {
+  // Setup Infrared Pins
+  pinMode(IR_1, INPUT);
+  pinMode(IR_2, INPUT);
+  pinMode(IR_3, INPUT);
+  pinMode(IR_4, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
