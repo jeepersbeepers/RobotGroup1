bf312d4d019115af64c222d4d56db64341788510
lachimolala-109 <95944246+lachimolala-109@users.noreply.github.com>
2024-05-06 11:12:25 -0400

    color sensor thigns

--- RobotGroup1/RobotGroup1.ino
+++ RobotGroup1/RobotGroup1.ino
@@ -1,7 +1,15 @@
+#include <tcs3200.h>
+
 #define IR_1 2
 #define IR_2 4
 #define IR_3 7
 #define IR_4 8
+
+#define COLOR_0 A0
+#define COLOR_1 A1
+#define COLOR_2 A2
+#define COLOR_3 A3
+#define COLOR_OUT A4
 
 unsigned long irSensorMillis = 0;  // Timer to track the lsat report of the IR sensors
 
